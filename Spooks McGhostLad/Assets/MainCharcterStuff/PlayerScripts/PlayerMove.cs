edd0b7fd8fb744d900e7c0dacfde3271dc04c3e3
adamdeere <adam.deere@hotmail.com>
2020-03-25 19:42:07 +0000

    added walking and jumping
    added walking functionalty from a ps4 controller. also added the animations for walking and jumping

---
+++ Spooks McGhostLad/Assets/MainCharcterStuff/PlayerScripts/PlayerMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] public CharacterController2D controler;
+
+    private float horizontalMove = 0;
+
+    public float runSpeed = 40;
+
+    private bool jump = false;
+    private bool crouch = false;
+
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("jumping", jump);
+        }
+
+        if (Input.GetAxisRaw("Crouch") == -1)
+        {
+            crouch = true;
+        }
+        else 
+        {
+            crouch = false;
+        }
+    }
+    public void OnLand()
+    {
+        animator.SetBool("jumping", false);
+    }
+    private void FixedUpdate()
+    {
+        controler.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
